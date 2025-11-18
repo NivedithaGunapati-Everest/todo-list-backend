3457573375d0dacefc3bb385fdf9f4ec4d81881c
NivedithaGunapati <niveditha.gunapati@everest.engineering>
2025-11-18 17:39:03 +0530

    Add routes to get tasks data

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { addTasks } from "../controllers/taskController";
+import { addTasks, getAllTasks } from "../controllers/taskController";
 
 export const router = Router();
 
 router.post("/task", addTasks);
+router.get("/tasks", getAllTasks);
