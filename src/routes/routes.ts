9f0a6463616f996dc5a7912f7730957d524c96e2
NivedithaGunapati <niveditha.gunapati@everest.engineering>
2025-11-19 11:04:25 +0530

    Update routes with task update route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
-import { addTasks, getAllTasks } from "../controllers/taskController";
+import {
+  addTasks,
+  getAllTasks,
+  updateTask,
+} from "../controllers/taskController";
 
 export const router = Router();
 
 router.post("/task", addTasks);
 router.get("/tasks", getAllTasks);
+router.put("/task/:id", updateTask);
