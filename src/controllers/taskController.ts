f2d821a5ec3d3e810b679551c4120ee98dc6c0a4
NivedithaGunapati <niveditha.gunapati@everest.engineering>
2025-11-19 13:42:32 +0530

    Add controller to delete specific task

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from "express";
-import { addTask, getTasks, updateTaskService } from "../services/taskServices";
+import {
+  addTask,
+  deleteTask,
+  getTasks,
+  updateTaskService,
+} from "../services/taskServices";
 
 export const addTasks = async (req: Request, res: Response) => {
   try {
@@ -43,5 +48,22 @@ export const updateTask = async (req: Request, res: Response) => {
     }
   } catch {
     res.status(500).json("Error while updating task");
+  }
+};
+
+export const deleteSpecificTask = async (req: Request, res: Response) => {
+  try {
+    const taskId = req.params.id;
+    if (taskId) {
+      const task = await deleteTask(taskId.toString());
+      if (task === false) {
+        res.status(404).json("The given id doesnot exist");
+      }
+      res.status(200).json("task deleted successfully");
+    } else {
+      res.status(400).json("Id is required in the url");
+    }
+  } catch {
+    res.status(500).json({ message: "Error while deleting task" });
   }
 };
